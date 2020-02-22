ecdffc1df5bbdfe33678893cb2ac8b5555af2709
Manasi Burange <burangemanasi@gmail.com>
2020-02-21 20:23:46 -0800

    Trees5 problem 1 Completed

---
+++ Problem102_PopulatingNextRightPointers.java
@@ -0,0 +1,113 @@
+//Time Complexity: O(n)
+//Space Complexity: No extra Space used
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    
+    public void getNextPtr(Node root) {
+    //edge case
+    if(root == null)
+        return;
+        
+    //iterative
+    if(root.left != null){
+        root.left.next = root.right;
+        if(root.next != null)
+            root.right.next = root.next.left;
+    }
+    
+    getNextPtr(root.left);
+    getNextPtr(root.right);
+    }
+    
+    public Node connect(Node root) {
+        
+    getNextPtr(root);
+    return root;
+    }
+}
+
+/*************************************************************** */
+//Time Complexity : O(n)
+//Space Complexity: O(log(n))
+//using Breadth First Search
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        //edge case
+        if (root == null) {
+            return root;
+        }
+        //creating que for level order traversal
+        Queue<Node> que = new LinkedList<Node>(); 
+        que.add(root);
+        
+        // Outer while loop which iterates over 
+        // each level
+        while (que.size() > 0) {
+            int size = que.size();            
+            for(int i = 0; i < size; i++) {
+                // Pop a node from the queue
+                Node node = que.poll();
+                if (i < size - 1) {
+                    node.next = que.peek();
+                }                
+                // Add the children, if any
+                //left child
+                if (node.left != null) {
+                    que.add(node.left);
+                }
+                //right child
+                if (node.right != null) {
+                    que.add(node.right);
+                }
+            }
+        }
+        return root;
+    }
+}
