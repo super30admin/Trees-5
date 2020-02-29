a9e507420d26a9b98aa9a80005291e6477f3a9cd
Sanjay Vishwakarma <svishwak@andrew.cmu.edu>
2020-02-28 21:37:50 -0800

    Populate Pointers in Tree Solution

---
+++ BigN102PopulatePointersInTree.java
@@ -0,0 +1,48 @@
+// Time complexity is O(n)
+// space complexity is O(n)
+// This solution is submitted on leetcode
+
+public class BigN102PopulatePointersInTree {
+	// Definition for a Node.
+	class Node {
+		public int val;
+		public Node left;
+		public Node right;
+		public Node next;
+
+		public Node() {
+		}
+
+		public Node(int _val) {
+			val = _val;
+		}
+
+		public Node(int _val, Node _left, Node _right, Node _next) {
+			val = _val;
+			left = _left;
+			right = _right;
+			next = _next;
+		}
+	};
+
+	class Solution {
+		public Node connect(Node root) {
+			// edge case
+			if (root == null)
+				return null;
+			Node temp = root;
+			while (temp.left != null) {
+				Node curr = temp;
+				while (curr != null) {
+					curr.left.next = curr.right;
+					if (curr.next != null) {
+						curr.right.next = curr.next.left;
+					}
+					curr = curr.next;
+				}
+				temp = temp.left;
+			}
+			return root;
+		}
+	}
+}
