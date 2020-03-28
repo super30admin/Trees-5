930086dd03d4ad1e4ecf3337c219b4b8cd279fad
priyanka1012-singhal <priyanka.singhal@sjsu.edu>
2020-03-28 00:44:24 -0700

    Add recursive and iterative solution - Populating next right pointer

--- Problem1.java
+++ Problem1.java
@@ -1,5 +1,5 @@
 // Problem1 Populating Next Right Pointers in Each Node(https://leetcode.com/problems/populating-next-right-pointers-in-each-node/)
-//Level order traversal
+//Level order traversal suing queue
 // Time - O(n)
 // Space - O(n)
 class Solution {
@@ -30,6 +30,55 @@ class Solution {
         
         return root;
     }
-    
-    
+}
+
+//Level order traversal iterative without using an extra DS
+// Time - O(n)
+// Space - O(1)
+class Solution {
+    public Node connect(Node root) {
+       if(root == null)
+           return null;
+         
+        
+        Node firstLevelNode = root;
+        while(firstLevelNode.left != null){
+            //Perfect BT so dnt have to do null check for right child
+            Node curr = firstLevelNode;
+            while(curr != null){
+               if(curr.left != null){
+                    curr.left.next = curr.right;
+                    if(curr.next != null){
+                        curr.right.next = curr.next.left;
+                    }
+                } 
+                curr = curr.next;
+            }
+            
+           firstLevelNode = firstLevelNode.left;
+        }
+        return root;
+    }
+}
+
+//Level order traversal recursive without using an extra DS
+// Time - O(n)
+// Space - O(Height of the tree)
+class Solution {
+    public Node connect(Node root) {
+        if(root == null)
+           return null;
+        //Perfect BT so dnt have to do null check for right child
+        if(root.left != null){
+            root.left.next = root.right;
+            if(root.next != null){
+                root.right.next = root.next.left;
+            }
+        }
+        
+        connect(root.left);
+        connect(root.right);
+        
+        return root;
+    }
 }
