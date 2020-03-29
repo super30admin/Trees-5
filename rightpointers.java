b8f0f75756e00a3c8bc92a60ae493eb39e659e00
Arnav Shah <arnav1996@gmail.com>
2020-03-29 12:25:36 -0700

    Completed right pointers problem

---
+++ rightpointers.java
@@ -0,0 +1,35 @@
+# Time complexity:O(n)
+# Space complexity: 0(n)
+# Did code run successfully on leetcode: yes
+# Any problem you faced: No
+
+
+
+
+
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return root;
+
+        Node firstNodelevel = root;
+
+        while(firstNodelevel.left != null){
+            Node cursor = firstNodelevel;
+
+
+            while(cursor != null){
+                cursor.left.next = cursor.right;
+                if(cursor.next != null){
+                    cursor.right.next = cursor.next.left;
+                }
+
+                cursor = cursor.next;
+            }
+
+            firstNodelevel = firstNodelevel.left;
+        }
+        return root;
+
+    }
+}
