2e97904bcf3d970c7a5eb85e7e2551ea8a7840b2
unknown <sachinsharma3191@gmail.com>
2020-04-11 15:03:48 -0700

    Code changes for Recover BST

--- RecoverBST.java
+++ RecoverBST.java
@@ -5,3 +5,57 @@
 
 
 // Your code here along with comments explaining your approach
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    TreeNode prev, first, middle, last;
+
+	public void recoverTree(TreeNode root) {
+		if (root != null) {
+			inorderTraversal(root);
+			if (first != null && last != null) {
+				int temp = first.val;
+				first.val = last.val;
+				last.val = temp;
+			}
+			// Adjacent nodes swapped
+			else if (first != null && middle != null) {
+				int temp = first.val;
+				first.val = middle.val;
+				middle.val = temp;
+			}
+		}
+	}
+
+	public void swap(int x, int y) {
+		int temp = x;
+		x = y;
+		y = temp;
+	}
+
+	public void inorderTraversal(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		inorderTraversal(root.left);
+		if (prev != null && root.val < prev.val) {
+			if (first == null) {
+				first = prev;
+				middle = root;
+			} else {
+				last = root;
+			}
+		}
+		prev = root;
+		inorderTraversal(root.right);
+	}
+
+
+}
