0fea3c1b92717608fde0c89e542b8957f482f4a0
gayathryR <gayathry.rpillai@gmail.com>
2020-05-08 15:19:14 -0700

    Solved Problem 1 and 2

---
+++ Program1.java
@@ -0,0 +1,54 @@
+//Time Complexity : O(n)
+//Space Complexity : O(number of leaf nodes)
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        
+        if(root==null){
+            return root;
+        }
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0; i<size;i++){
+                Node temp = queue.poll();
+                if(!queue.isEmpty() && i!=size-1){
+                    temp.next = queue.peek();
+                }
+                if(temp.left!=null){
+                    queue.offer(temp.left);
+                }
+                if(temp.right!=null){
+                    queue.offer(temp.right);
+                }
+            }
+             
+        }
+        return root;
+        }
+    }
