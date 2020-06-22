1d831fd5b8e8e6028e08da5bc9e44e912edfbd7a
albertcoder <eralbert9191@gmail.com>
2020-06-22 14:32:32 -0700

    solving using pointers and no extra space

--- nextPointer.java
+++ nextPointer.java
@@ -1,6 +1,6 @@
+//APPROACH 1: BFS level order traversal
 //time complexity O(n) since we are visiting each node
 //space complexity O(n) since the max size of queue is n/2
-//APPROACH BFS level order traversal
 
 /*
 // Definition for a Node.
@@ -32,5 +32,41 @@ class Solution {
             }
         }
        return root;
+    }
+}
+
+
+//APPROACH 2: Without using extra space. Just manipulate the pointers while iterating over nodes.
+//time complexity O(n)
+//space complexity O(1)
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return null;
+        Node level = root;
+        while(level.left != null){
+            Node curr = level;
+            while(curr != null){
+                curr.left.next = curr.right;
+                if(curr.next != null){
+                    curr.right.next = curr.next.left;
+                }
+                curr = curr.next;
+            }
+            level = level.left;
+        }
+        return root;
     }
 }
