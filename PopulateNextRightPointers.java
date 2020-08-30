ec37f466ef743150d1df4ebfa732779339985de4
Arpit Sharma <arpit317@gmail.com>
2020-08-30 02:04:12 -0700

    Update PopulateNextRightPointers.java

--- PopulateNextRightPointers.java
+++ PopulateNextRightPointers.java
@@ -21,6 +21,8 @@ class Node {
 };
 */
 
+//TC: O(n)
+//SC: O(n)
 class Solution {
     public Node connect(Node root) {
         if(root==null) return null;
@@ -38,6 +40,27 @@ class Solution {
                         q.add(curr.right);
                 }
             }
+        }
+        return root;
+    }
+}
+
+//TC: O(n)
+//SC: O(1)
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return root;
+        Node level = root;
+        while(level.left != null){
+            Node curr = level;
+            while(curr != null){
+                curr.left.next = curr.right;
+                if(curr.next!=null){
+                    curr.right.next = curr.next.left;
+                }
+                curr = curr.next;
+            }
+            level = level.left;
         }
         return root;
     }
