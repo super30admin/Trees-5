e2a2d068fb6712f6219a345e493c7e5b3e2b563c
Arpit Sharma <arpit317@gmail.com>
2020-08-30 02:31:28 -0700

    Update PopulateNextRightPointers.java

--- PopulateNextRightPointers.java
+++ PopulateNextRightPointers.java
@@ -65,3 +65,24 @@ class Solution {
         return root;
     }
 }
+
+
+//TC: O(n)
+//SC: O(n)
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return root;
+        dfs(root.left, root.right);
+        return root;
+    }
+    
+    private void dfs(Node left, Node right){
+        if(left == null) return;
+        
+        left.next = right;
+        
+        dfs(left.left, left.right);
+        dfs(left.right, right.left);
+        dfs(right.left, right.right);
+    }
+}
