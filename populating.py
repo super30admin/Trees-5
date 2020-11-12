1d89a13c95a30449b35044b8f75e2462ded63f68
SowmyaJupally <63617870+SowmyaJupally@users.noreply.github.com>
2020-11-11 18:22:18 -0800

    Trees-5 Done

---
+++ populating.py
@@ -0,0 +1,29 @@
+#Time Complexity: O(N)
+#Space Complexity: O(1)
+#Run on Leetcode: Yes
+#Any Issues: No
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        
+        if not root:
+            return root
+        
+      
+        leftmost = root
+        
+        while leftmost.left:
+            
+            head = leftmost
+            while head:
+                
+                head.left.next = head.right
+                
+                if head.next:
+                    head.right.next = head.next.left
+                
+                head = head.next
+            
+            leftmost = leftmost.left
+        
+        return root 
