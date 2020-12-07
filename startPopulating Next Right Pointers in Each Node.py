c6ec9d29c1cd8ba0fd844e39502692ddc7770166
Vijay Raja <jobprepnyu@gmail.com>
2020-12-07 12:47:56 -0500

    Update startPopulating Next Right Pointers in Each Node.py

--- startPopulating Next Right Pointers in Each Node.py
+++ startPopulating Next Right Pointers in Each Node.py
@@ -1,29 +1,41 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
 class Solution:
-    def recoverTree(self, root: TreeNode) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
-     
-        def inorderTraversal(node):
-            nonlocal first, second , prev
-            if node is None:
-                return 
-            inorderTraversal(node.left)
-            if prev and node.val<prev.val:
-                second = node
-                if first is None:
-                    first = prev
-                else:
-                    return 
-            prev = node
-            inorderTraversal(node.right)
-        
-        first = second = prev = None
-        inorderTraversal(root)
-        first.val , second.val = second.val, first.val
+    def connect(self, root: 'Node') -> 'Node':
+    #Recursive
+        def helper(node):
+            
+            if not node or not node.right:
+                return
+            node.left.next = node.right
+            if node.next:
+                if node.right:
+                    node.right.next = node.next.left
+            helper(node.left)
+            helper(node.right)
+        helper(root)
+        return root
+    
+    
+    #Iterative
+        if not root:
+            return root
+        start = root
+        while start.left:
+            node = start
+            while node:
+                node.left.next = node.right
+                if node.next:
+                    node.right.next = node.next.left
+                node = node.next
+            start = start.left
+        return root
+            
