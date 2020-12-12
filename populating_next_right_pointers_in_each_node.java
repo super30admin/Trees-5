df63af70375a65c097107fe25f49dfa8983258a4
Sumit Janawlekar <janawlekar.s@husky.neu.edu>
2020-12-12 16:29:20 -0500

    Constant space solution for  populating_next_right_pointers_in_each_node problem

--- populating_next_right_pointers_in_each_node.java
+++ populating_next_right_pointers_in_each_node.java
@@ -1,14 +1,13 @@
 // Time Complexity : O(n), where n is the number of nodes in the tree
-// Space Complexity : O(n), where n is the number of nodes in the tree (since it is perfect binary tree, there will atmost be n/2
-                        // nodes in the queue)
+// Space Complexity :O(1), no extra space used
 // Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : Correct me on the space complexity
+// Any problem you faced while coding this :
 
-//Three Liner approcah of your code in plain English
-//Perform BFS starting from the root node.
-//Maintain a size variable to process all the nodes together at the same level
-//Till queue is not empty remove the element add all its children and populate the next pointer from the current node to the node
-        //on the top of the queue (Only if the current node is not the last node at that level)
+//Three liner approach of your code in plain English
+//1. Take 2 pointers, one to track the level and one to track all the nodes at one level
+//2. At each level, at the current node, populate a next right pointer from current's left node to the right node AND populate the next right 
+        //pointer from cuurent's right node to currrent's next siblings's left node (If next sibling exists)
+//. Continue doing this till level has not reached the leaf level (level.left != null)
 
 // Your code here along with comments explaining your approach
 
@@ -39,28 +38,27 @@ class Solution {
     public Node connect(Node root) {
         //edge case
         if(root == null) return null;
+        Node level = root;
         
-        //create a queue to perform BFS
-        Queue<Node> bfs = new LinkedList<>();
-        //add the root to the queue to start BFS
-        bfs.add(root);
-        
-        while(!bfs.isEmpty()){
-            //get the queue size, to process all the nodes together at the same level
-            int size = bfs.size();
-            for(int i=0; i<size; i++){
-                //pop the element from the queue
-                Node curr = bfs.poll();
-                //add all the childs of the node in the queue
-                if(curr.left != null){
-                    bfs.add(curr.left);
-                    bfs.add(curr.right);
+        //traverse all the levels of the binary tree
+        while(level.left != null){
+            //2 pointers, one for going to the next level and one for going to all the      nodes at that level
+            //Node level = root;
+            Node curr = level;
+            
+            //traverse all the nodes at each level
+            while(curr != null){
+                //populate the next right pointer from the left child to the right child of curr
+                curr.left.next = curr.right;
+                //populate the next right pointer from the current's right child to the current's sibbling's left child
+                if(curr.next != null){
+                    curr.right.next = curr.next.left;
                 }
-                //populate the next right pointer, if the node is not the last node at that level
-                if(i < size-1){
-                    curr.next = bfs.peek();
-                }
+                //move current to the next node in the same level
+                curr = curr.next;
             }
+            //move level to the next level in the tree
+            level = level.left;
         }
         return root;
     }
