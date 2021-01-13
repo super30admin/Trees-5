7bd433c57a4e8e720d550fc5de31144c47239a4c
Rishi Raj <rishiraj824@gmail.com>
2021-01-12 21:44:21 -0500

    Update right-next-pointer.java

--- right-next-pointer.java
+++ right-next-pointer.java
@@ -22,3 +22,37 @@ class Solution {
         dfs(right.left, right.right);
     }
 }
+
+// BFS Brute Force
+// Time - O(N)
+// Space - O(N)
+
+class Solution {
+    public Node connect(Node root) {
+         if(root == null) {
+            return root;
+        }
+        
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for(int i=0;i<size;i++) {
+                Node cur = q.poll(); 
+                if(i!=size-1) {
+                    cur.next = q.peek();
+                }
+                if(cur.left!=null) {
+                    q.add(cur.left);   
+                }
+                if(cur.right!=null) {
+                    q.add(cur.right);                    
+                }
+            }
+        }
+        
+        return root;
+        
+    }
+}
