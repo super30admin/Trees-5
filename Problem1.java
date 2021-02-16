70b490856f14cf1035b2f04f7823e2b54e7ac14b
Yasunori Iwayanagi <yiwayanagi@upward.net>
2021-02-16 10:40:46 -0800

    Submission Trees 5

---
+++ Problem1.java
@@ -0,0 +1,33 @@
+//Time complexity: O(N)
+//Space complexity; O(1)
+
+class Solution {
+    public Node connect(Node root) {
+        
+        if (root == null) {
+            return root;
+        }
+        
+        Node leftmost = root;
+        
+        while (leftmost.left != null) {
+            
+            Node head = leftmost;
+            
+            while (head != null) {
+                
+                head.left.next = head.right;
+                
+                if (head.next != null) {
+                    head.right.next = head.next.left;
+                }
+                
+                head = head.next;
+            }
+            
+            leftmost = leftmost.left;
+        }
+        
+        return root;
+    }
+}
