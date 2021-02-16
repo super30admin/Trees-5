bd31182c1396a988d8daaed79a7dde3fe972a37b
Sachin Devatar <73754947+SachinDevatar77@users.noreply.github.com>
2021-02-16 14:17:17 -0600

    Update Populating_Next_Right.py

--- Populating_Next_Right.py
+++ Populating_Next_Right.py
@@ -1,5 +1,6 @@
 # Time: O(N) - BFS approach without creating a queue
 # Space: O(1) - BFS without queue space
+# With the help of curr we are connecting the pointer by level by level
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
@@ -8,7 +9,7 @@ class Solution:
         level = root
         while level.left != None:
             curr = level
-            while curr != None  :
+            while curr != None:
                 curr.left.next = curr.right
                 if curr.next != None:
                     curr.right.next = curr.next.left
