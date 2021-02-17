d4e6c2d3d6a8d4d498ddbdb402e829d0927fcc86
deepashree <dkulkarni2@scu.edu>
2021-02-16 17:29:28 -0800

    Solved Question-1

---
+++ PopulatingNextRightPointers.java
@@ -0,0 +1,88 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+// TC: O(N) , N -> no. of nodes of the tree
+// SC: O(1) 
+// Did it run successfully on Leetcode? :Yes
+class Solution {
+    public Node connect(Node root) {
+        if ( root == null)
+            return null;
+        Node level = root;
+        while (level.left != null)
+        {
+            Node curr = level;
+            while ( curr != null)
+            {
+                curr.left.next = curr.right;
+                if ( curr.next != null)
+                {
+                   curr.right.next = curr.next.left;
+                }
+                curr = curr.next;
+            }
+            level = level.left;
+        }
+        return root;
+    }
+}
+
+// TC: O(N) , N -> no. of nodes of the tree
+// SC: O(N) , size of queue -> no. of nodes of the tree
+// Did it run successfully on Leetcode? :Yes
+// class Solution {
+//     public Node connect(Node root) {
+//           if ( root == null)
+//             return null;
+//         Queue<Node> q = new LinkedList();
+//         q.offer(root);
+//         while (!q.isEmpty())
+//         {
+//             int size = q.size();
+//             Node prev = q.poll();
+//             if (prev.left != null)
+//             {
+//                 q.offer(prev.left);
+//             }
+//             if (prev.right != null)
+//             {
+//                 q.offer(prev.right);
+//             }
+//             for ( int i = 1; i < size; i++)
+//             {    
+//                Node curr = q.poll();
+//                  if (curr.left != null)
+//                  {
+//                   q.offer(curr.left);
+//                  } 
+//                  if (curr.right != null)
+//                  {      
+//                      q.offer(curr.right);
+//                  }
+//                prev.next = curr;
+//                prev = curr;
+//             }
+//         }
+//         return root;
+//     }
+// }
