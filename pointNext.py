596786b09c2548711f2e1555649e0d6205d26b85
Shantanu Neema <38290435+shantanuneema@users.noreply.github.com>
2021-02-20 07:48:45 -0800

    Update pointNext.py

--- pointNext.py
+++ pointNext.py
@@ -42,3 +42,43 @@ class Solution:
             nodes = node_list
 
         return root
+
+# Optimized solution (BFS)
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    
+    """
+    Description: Populate next right pointers in each node
+    
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    
+    Approach: 
+    - use nodes at each level of the tree (left side)
+    - for each level rewire the .next pointer for children of the node
+    - continue until there is no more nodes on left of last level
+    """
+    
+    def connect(self, root: 'Node') -> 'Node':
+            
+        if root == None: return
+        
+        level = root
+        while level.left:
+            curr = level
+            while curr:
+                curr.left.next = curr.right
+                if curr.next != None:
+                    curr.right.next = curr.next.left
+                curr = curr.next
+            level = level.left
+        return root
