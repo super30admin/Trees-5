63b4c56067352f88e35d020798fb2cee330fa052
Skanda Suresh <skoct13@gmail.com>
2021-02-23 20:15:29 -0700

    completed nextPointer

---
+++ nextPointer.py
@@ -0,0 +1,33 @@
+# TC: O(N)
+# SC: O(1) recursive stack
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        
+        if not root:
+            return root
+        
+        level = root
+        ans = level
+        
+        while level.left:
+            
+            cur = level
+            while cur:
+                cur.left.next = cur.right
+                if cur.next:
+                    cur.right.next = cur.next.left
+                cur = cur.next
+            level = level.left
+            
+        return ans
