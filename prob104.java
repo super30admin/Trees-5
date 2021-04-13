833c1a66ac43ed580e81dbaa4bec239b9f80ff2e
stuti <stuti@DESKTOP-1KQ9LMO>
2021-04-13 19:41:30 -0400

    done

---
+++ prob104.java
@@ -0,0 +1,20 @@
+//TC:O(n)
+//SC:O(1)
+class Solution {
+    public Node connect(Node root) {
+        if(root== null)return null;
+        Node level= root;
+        while(level.left !=null){
+            Node curr = level;
+            while(curr !=null){
+                curr.left.next = curr.right;
+                if(curr.next !=null){
+                    curr.right.next= curr.next.left;
+                }
+                curr= curr.next;
+            }
+            level = level.left;
+        }
+        return root;
+    }
+}
