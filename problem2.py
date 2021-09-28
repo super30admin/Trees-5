34c04f21ae78b8025b6036c84892386184fd95c9
Yash Naredi <yashnaredi26@gmail.com>
2021-09-28 11:37:38 -0400

    Completed Trees-5

---
+++ problem2.py
@@ -0,0 +1,56 @@
+#TC:O(N)
+#SC: O(1)
+class Solution:
+    def recoverTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        # predecessor is a Morris predecessor. 
+        # In the 'loop' cases it could be equal to the node itself predecessor == root.
+        # pred is a 'true' predecessor, 
+        # the previous node in the inorder traversal.
+        x = y = predecessor = pred = None
+        
+        while root:
+            # If there is a left child
+            # then compute the predecessor.
+            # If there is no link predecessor.right = root --> set it.
+            # If there is a link predecessor.right = root --> break it.
+            if root.left:       
+                # Predecessor node is one step left 
+                # and then right till you can.
+                predecessor = root.left
+                while predecessor.right and predecessor.right != root:
+                    predecessor = predecessor.right
+ 
+                # set link predecessor.right = root
+                # and go to explore left subtree
+                if predecessor.right is None:
+                    predecessor.right = root
+                    root = root.left
+                # break link predecessor.right = root
+                # link is broken : time to change subtree and go right
+                else:
+                    # check for the swapped nodes
+                    if pred and root.val < pred.val:
+                        y = root
+                        if x is None:
+                            x = pred 
+                    pred = root
+                    
+                    predecessor.right = None
+                    root = root.right
+            # If there is no left child
+            # then just go right.
+            else:
+                # check for the swapped nodes
+                if pred and root.val < pred.val:
+                    y = root
+                    if x is None:
+                        x = pred 
+                pred = root
+                
+                root = root.right
+        
+        x.val, y.val = y.val, x.val
