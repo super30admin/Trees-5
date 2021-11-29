e793107bb75e6f6d1bd0b03dbf535fda65c366df
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2021-11-28 20:42:09 -0500

    Create Populating_Next_Right_Pointers_in_Each_Node.java

---
+++ Populating_Next_Right_Pointers_in_Each_Node.java
@@ -0,0 +1,58 @@
+//Bruteforce:
+//TC: O(N)
+//SC: O(N)
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        
+        if(root == null) return null;
+        
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            Node prev = q.poll();
+            if(prev.left != null){
+                q.add(prev.left); q.add(prev.right);
+            }
+        
+            Node curr;
+            for(int i = 1; i< size; i++){
+                curr = q.poll();
+                prev.next = curr;
+                if(curr.left != null){
+                    q.add(curr.left); q.add(curr.right);
+                }
+                prev = curr;
+            }
+            }
+        
+        return root;
+        
+    }
+    
+}
