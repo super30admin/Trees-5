f3252e199a7d162499ed42c2742cf063d2970233
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2021-11-28 22:12:42 -0500

    Update Populating_Next_Right_Pointers_in_Each_Node.java

--- Populating_Next_Right_Pointers_in_Each_Node.java
+++ Populating_Next_Right_Pointers_in_Each_Node.java
@@ -104,3 +104,50 @@ class Solution {
         return root;
     }
 }
+
+//DFS solution
+//TC: O(N)
+//SC: O(1)
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return null;
+        dfs(root.left, root.right);
+            return root;
+        
+    }
+    private void dfs(Node left, Node right){
+        //base
+        if(left == null) return;
+        //logic
+        left.next = right;
+        dfs(left.left, left.right);
+        dfs(left.right, right.left);
+        dfs(right.left, right.right);
+    }
+}
+
+
