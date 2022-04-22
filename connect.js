bb50106cb60839e926fb79e19be3344517e0f852
Krina Patel <kp42942n@pace.edu>
2022-04-22 15:51:57 -0400

    Create connect.js

---
+++ connect.js
@@ -0,0 +1,33 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+
+var dfs =function(root) {
+    if(root.left === null) return;
+    root.left.next = root.right;
+    if(root.next !== null) root.right.next = root.next.left;
+    dfs(root.left);
+    dfs(root.right);
+}
+
+var connect = function(root) {
+    if(root === null) return null;
+    dfs(root);
+    return root;
+};
