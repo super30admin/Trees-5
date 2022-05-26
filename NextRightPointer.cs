696915f5b7e1579542fcb0b8baf4f89c1581f354
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-05-25 22:44:23 -0700

    Add files via upload

---
+++ NextRightPointer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    class NextRightPointer
+    {
+        /*
+         * T.C: O(N) since traversing all the nodes
+         * S.C: O(1) no extra space used
+         */
+        public TreeNode Connect(TreeNode root)
+        {
+            if (root == null) return null;
+
+            TreeNode level = root;
+
+
+            while (level.left != null)
+            {
+                TreeNode curr = level;
+                while (curr != null)
+                {
+                    curr.left.next = curr.right;
+                    if (curr.next != null)
+                    {
+                        curr.right.next = curr.next.left;
+                    }
+                    curr = curr.next;
+                }
+                level = level.left;
+            }
+
+            return root;
+        }
+
+
+        //Method2
+        /* T.C: O(n) since traversing all the nodes
+         * S.C: O(h) hight of the tree
+         */
+        public TreeNode Connect1(TreeNode root)
+        {
+            if (root == null) return null;
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (q.Count != 0)
+            {
+                int size = q.Count;
+                TreeNode curr = q.Dequeue();
+
+                if (curr.left != null)
+                {
+                    q.Enqueue(curr.left);
+                }
+                if (curr.right != null)
+                {
+                    q.Enqueue(curr.right);
+                }
+
+                for (int i = 1; i < size; i++)
+                {
+                    TreeNode temp = q.Dequeue();
+                    curr.next = temp;
+                    curr = temp;
+
+                    if (curr.left != null)
+                    {
+                        q.Enqueue(curr.left);
+                    }
+                    if (curr.right != null)
+                    {
+                        q.Enqueue(curr.right);
+                    }
+                }
+            }
+            return root;
+        }
+    }
+}
---
+++ RecoverBinaryTree.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public class RecoverBinaryTree
+    {
+        /*
+         * T.C: O(n) since traversing all the element of the tree
+         * S.C: O(h) high of the tree
+         */
+        TreeNode first;
+        TreeNode second;
+        TreeNode prev;
+        bool flag = false;
+        public void RecoverTree(TreeNode root)
+        {
+            if (root == null) return;
+
+            inorder(root);
+
+            int temp = first.val;
+            first.val = second.val;
+            second.val = temp;
+        }
+
+        public void inorder(TreeNode root)
+        {
+            if (root == null)
+                return;
+
+            inorder(root.left);
+
+            if (prev != null && prev.val > root.val)
+            {
+                if (first == null)
+                {
+                    first = prev;
+                    second = root;
+                    flag = true;
+                }
+                else
+                {
+                    second = root;
+                    flag = false;
+                }
+            }
+            prev = root;
+            if (!(flag == false && first != null && second != null))
+            {
+                inorder(root.right);
+            }
+        }
+    }
+}   
