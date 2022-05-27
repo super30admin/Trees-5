6934d2fb99fd729427a7f69c24fee708ef5b7ee2
skatkar <skatkar225@gmail.com>
2022-05-27 17:29:45 -0500

    Trees-5 submission

---
+++ NextRightPointer.java
@@ -0,0 +1,48 @@
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+
+public class NextRightPointer {
+
+    // TC : O(n)
+    // SC : O(1)
+    public Node connect(Node root) {
+        if(root == null) return null;
+
+        Node level = root;
+        Node current;
+
+        while(level.left != null) {
+            current = level;
+            while(current != null) {
+                current.left.next = current.right;
+
+                if(current.next != null)
+                    current.right.next = current.next.left;
+
+                current = current.next;
+            }
+
+            level = level.left;
+        }
+
+        return root;
+    }
+}
