d9abefe86bf71befcd5e833f6933ef8185e0bde6
Rujal <rujal8452@gmail.com>
2022-05-29 03:19:47 -0400

    Problem1

---
+++ Problem1.java
@@ -0,0 +1,56 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Problem1 {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    // BFS Solution
+    // TC : O(n)
+    // SC : O(h) // h is height of tree
+    public Node connect(Node root) {
+        if (root == null) return root;
+
+        Queue<Node> que = new LinkedList<>();
+        que.add(root);
+
+        while (!que.isEmpty()) {
+            int size = que.size();
+            Node curr = que.poll();
+            if (curr.left != null) {
+                que.add(curr.left);
+                que.add(curr.right);
+            }
+            for (int i = 1; i < size; i++) {
+                Node temp = que.poll();
+                curr.next = temp;
+                curr = temp;
+                if (curr.left != null) {
+                    que.add(curr.left);
+                }
+                if (curr.right != null) {
+                    que.add(curr.right);
+                }
+            }
+        }
+        return root;
+    }
+}
