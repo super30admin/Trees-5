f5db1fd3f8a99be411c7a6f9a85a0af06975242f
Harsh <akshatrana2007@gmail.com>
2022-06-28 16:29:55 -0400

    completed assignment

---
+++ leet116.js
@@ -0,0 +1,16 @@
+TC - O(N)
+SC - O(1)
+
+var connect = function(root) {
+    let ptr = root;
+    while(root && root.left){
+        let temp = root;
+        while(temp) {
+            temp.left.next = temp.right;
+            temp.right.next = temp.next ? temp.next.left : null;
+            temp = temp.next;
+        }
+        root = root.left;
+    }
+    return ptr;
+}
