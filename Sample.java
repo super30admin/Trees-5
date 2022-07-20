457e99c3779ac0e39b40d9b7cebb9f5e8bb04579
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-07-20 11:35:33 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -66,3 +66,134 @@ class Solution {
         inorder(root.right);
     }
 }
+
+//****104.116. POPULATING NEXT RIGHT POINTERS IN EACH NODE-  BRUTEFORCE BFS****
+//Time complexity:0(n);
+//Space Complexity:0(n); 
+//Leetcode runnable:Y
+//Any doubts:N;
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        //null case
+        if(root==null)
+        {
+            return null;
+        }
+        
+        //Making queue for performing bfs
+        Queue<Node> q=new LinkedList<>();
+        
+        //Adding root to the queue
+        q.add(root);
+        
+        //Playing with queue
+        while(!q.isEmpty())
+        {
+            int size=q.size();
+            //Initialize a prev
+            Node prev=null;
+            //Iterating through queue
+            for(int i=0;i<size;i++)
+            {
+                Node curr=q.poll();
+                if(i!=0)
+                {
+                    prev.next=curr;
+                }
+                if(curr.left!=null)
+                {
+                    q.add(curr.left);
+                    q.add(curr.right);
+                }
+                prev=curr;
+            }
+        }
+        return root;
+        
+        
+    }
+}
+
+
+//****104.116. POPULATING NEXT RIGHT POINTERS IN EACH NODE-  OPTIMIZED BFS****
+//Time complexity:0(n);
+//Space Complexity:0(1); 
+//Leetcode runnable:Y
+//Any doubts:N;
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        //null case
+        if(root==null)
+        {
+            return null;
+        }
+        
+        Node level=root;
+        
+        while(level.left!=null)
+        {
+            Node curr=level;
+            while(curr!=null)
+            {
+                curr.left.next=curr.right;
+                if(curr.next!=null)
+                {
+                    curr.right.next=curr.next.left;
+                }
+                curr=curr.next;
+            }
+            level=level.left;
+        }
+        return root;
+        
+        
+    }
+}
