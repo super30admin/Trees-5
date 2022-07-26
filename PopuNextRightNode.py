5b90fdce0daf33cfe40dbfeb301b005e3dd48321
tathyapatel <92098849+tathyapatel@users.noreply.github.com>
2022-07-26 13:10:59 -0500

    Create PopuNextRightNode.py

---
+++ PopuNextRightNode.py
@@ -0,0 +1,26 @@
+#116. Populating Next Right Pointers in Each Node
+"""
+Time Complexity : O(n)
+Space Complexity : O(h)
+"""
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        if root.right: 
+            root.left.next = root.right
+            if root.next:
+                root.right.next = root.next.left
+        self.connect(root.left)
+        self.connect(root.right)
+        return root
