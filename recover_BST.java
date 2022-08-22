c5ccf3958820682c5daeab50a03ca209d3d0db4d
siddbanerjee2012 <sidd.banerjee2012@gmail.com>
2022-08-22 12:19:49 -0500

    Completed Sprint Trees-5

---
+++ recover_BST.java
@@ -0,0 +1,42 @@
+// Time Complexity : O(n)
+// Space Complexity : O(h)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    TreeNode prev;
+    TreeNode first, second;
+
+    public void recoverTree(TreeNode root) {
+        if (root == null)
+            return;
+        inorder(root);
+        // swapping after finding mismatch
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorder(TreeNode root) {
+        // base
+        if (root == null)
+            return;
+
+        // logic
+        inorder(root.left);
+        if (prev != null && prev.val >= root.val) {
+            if (first == null) {
+                first = prev;
+                second = root;
+            } else {
+                second = root;
+            }
+        }
+        prev = root;
+        inorder(root.right);
+
+    }
+}
