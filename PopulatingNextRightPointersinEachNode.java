36351467417f9f3c33b6aa368916fa65a99c6bf8
kunalwvm <107076313+kunalwvm@users.noreply.github.com>
2022-08-28 07:57:36 -0700

    Add files via upload

---
+++ PopulatingNextRightPointersinEachNode.java
@@ -0,0 +1,84 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+// 0(n)
+// 0(n)
+/*
+class Solution {
+    public Node connect(Node root) {
+        if(root == null)
+        {
+            return null;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty())
+        {
+        int size=q.size();
+        Node curr=q.poll();
+            if(curr.left!=null)
+            {
+                q.add(curr.left);
+                q.add(curr.right);
+            }
+            for(int i=1;i<size;i++)
+            {
+                Node next =q.poll();
+                curr.next=next;
+                if(next.left!=null)
+            {
+                q.add(next.left);
+                q.add(next.right);
+            }
+                curr=next;
+
+            }
+    }
+        return root;
+}
+}*/
+class Solution {
+    public Node connect(Node root) {
+        if(root == null)
+        {
+            return null;
+        }
+        Node lvl= root;
+        Node curr=null;
+        while(lvl.left!=null)
+        {
+            curr=lvl;
+            while(curr!=null)
+            {
+                curr.left.next=curr.right;
+                if(curr.next!=null)
+                {
+                    curr.right.next=curr.next.left;
+                }
+                curr=curr.next;
+            }
+            lvl=lvl.left;
+        }
+        return root;
+    }
+}
+
