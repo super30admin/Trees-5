12b042383b1ca4fa15b92057730704032196ef1d
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-11 22:51:21 -0700

    Update PopulatingNextRightPointers.java

--- PopulatingNextRightPointers.java
+++ PopulatingNextRightPointers.java
@@ -64,6 +64,31 @@ class Solution {
 }
 
 
+
+//Approach - 3.1 : dfs
+//Time Complexity : O(n)
+//Space Complexity : O(h)
+class Solution {
+    public Node connect(Node root) {        
+        if(root == null) return null;
+        dfs(root.left, root.right);
+        return root;
+    }
+    private void dfs(Node left, Node right){
+        //base
+        if(left == null) return;
+        
+        left.next = right;
+        //logic
+        dfs(left.left, left.right);
+        dfs(left.right, right.left);
+        dfs(right.left, right.right);
+        
+    }
+}
+
+
+
 //Approach - 1: Queue, prev, curr 
 //Time Complexity : O(n)
 //Space Complexity : O(n)
