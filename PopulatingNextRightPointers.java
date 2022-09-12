87264d3875c359871af6c7495f37fbd44bfd1716
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-11 18:46:07 -0700

    Update PopulatingNextRightPointers.java

--- PopulatingNextRightPointers.java
+++ PopulatingNextRightPointers.java
@@ -32,6 +32,36 @@ class Solution {
 }
 
 
+//Approach - 1.3: Queue, prev, curr  ***
+//Time Complexity : O(n)
+//Space Complexity : O(n)
+class Solution {
+    public Node connect(Node root) {
+        
+        if(root == null) return null;
+        
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            Node prev = null;
+            for(int i=0; i<size; i++){
+                Node curr = q.poll(); 
+                curr.next = prev;
+                //add the childrens
+                if(curr.left != null) q.add(curr.right);
+                if(curr.right != null) q.add(curr.left);
+                prev = curr;       
+            }
+         }
+         
+         return root;
+    }
+}
+
+
 
 //Approach - 1.1: Queue, prev, curr 
 //Time Complexity : O(n)
