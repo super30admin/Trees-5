bc4ed5620585c801df3be04e217be0f4af585e91
PremAmanchi <101784948+PremAmanchi@users.noreply.github.com>
2023-02-21 00:45:52 -0700

    Completed Trees5 - Please Evaluate

---
+++ recoverBST.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    TreeNode prev, first, second;
+
+    public void recoverTree(TreeNode root) {
+        if (root == null)
+            return;
+        inorder(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorder(TreeNode root) {
+        if (root.left != null)
+            inorder(root.left);
+        if (prev != null && root.val < prev.val) {
+            if (first == null)
+                first = prev;
+            second = root;
+        }
+        prev = root;
+        if (root.right != null)
+            inorder(root.right);
+    }
+}
