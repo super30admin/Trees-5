611742d781ba6e61e58afee2fe85b0b5434f8ace
Manthan Wadiwala <manthancar@gmail.com>
2023-03-20 12:24:01 -0400

    Completed Trees-5

---
+++ PopulatingRightNextPTRInEachNode.java
@@ -0,0 +1,23 @@
+// TC = O(N)
+// SC = O(1)
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return root;
+        Node level = root;
+
+        while(level.left != null){
+            Node curr = level;
+
+            while(curr != null){
+                curr.left.next = curr.right;
+                if(curr.next != null){
+                    curr.right.next = curr.next.left; 
+                }
+                curr = curr.next; 
+            }        
+            level = level.left;            
+        }
+        return root;
+    }
+}
