e6d936d1433eaaf8977d37b61e886fe475839fec
bhaveshbathija <65425830+bhaveshbathija@users.noreply.github.com>
2023-08-01 16:09:42 +0530

    completed trees-5

---
+++ RecoverBst.java
@@ -0,0 +1,33 @@
+class RecoverBst {
+    private TreeNode first;
+    private TreeNode second;
+    private TreeNode prev;
+
+    public void recoverTree(TreeNode root) {
+        if (root == null)
+            return;
+        inorder(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorder(TreeNode root) {
+        // base
+        if (root == null)
+            return;
+        // logic
+        inorder(root.left);
+        if (prev != null && prev.val >= root.val) {
+            // first breach
+            if (first == null) {
+                first = prev;
+                second = root;
+            } else {// 2nd breach
+                second = root;
+            }
+        }
+        prev = root;
+        inorder(root.right);
+    }
+}
