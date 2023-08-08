a8880a066a0c260becd7d129521a2f268289d968
Debdyuti Das <debdyuti@Debdyutis-MacBook-Air.local>
2023-08-08 13:08:31 -0700

    Added trees5

---
+++ Problem1.java
@@ -0,0 +1,23 @@
+//Time complexity is O(n)
+//Space complexity is O(1)
+
+public class Populate {
+    public Node connect(Node root) {
+        if(root == null) return root;
+        Node level = root;
+        while(level.left != null) {
+            Node curr = level;
+            while(curr != null) {
+                curr.left.next = curr.right;
+                if(curr.next != null) {
+                    curr.right.next = curr.next.left;
+                }
+                curr = curr.next;
+            }
+            level = level.left;
+        }
+        return root;
+}
+
+
+
