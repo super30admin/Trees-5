6b3d5b451bf4660b2c625fdb64aab1053aa3b1af
sharvanitamaskar <sharvanitamaskar@gmail.com>
2023-09-21 13:02:20 -0400

    trees5

---
+++ MorrisInorderTraversal.java
@@ -0,0 +1,26 @@
+#TC: O(n)
+#SC: O(1)
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left == null){
+                result.add(curr.val);
+                curr = curr.right;
+            } else {
+                TreeNode pre = curr.left;
+                while(pre.right != null && pre.right != curr){
+                    pre = pre.right;
+                }
+                if(pre.right == null){
+                    pre.right = curr;
+                    curr = curr.left;
+                } else {
+                   result.add(curr.val);
+                    curr = curr.right;
+                    pre.right = null;
+                } } }
+        return result;
+    } }
