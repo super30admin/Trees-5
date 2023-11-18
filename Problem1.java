316a1a56340e05a34dced37e43e7ef148697a0c5
Aditya Kondepudi <36687530+adityakondepudi@users.noreply.github.com>
2023-11-17 22:08:09 -0500

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,23 +1,21 @@
-//Time Complexity : O(n)
-//Space Complexity: O(logn)
+//Time Complexity: O(n)
+//Space Complexity:O(1)
 
 class Solution {
+
     public Node connect(Node root) {
         if(root == null) return null;
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                Node curr = q.poll();
-                if(i!=size-1){
-                    curr.next = q.peek();
+        Node level = root;
+        while(level.left!=null){
+            Node current = level;
+            while(current!=null){
+                current.left.next = current.right;
+                if(current.next != null){
+                    current.right.next = current.next.left;
                 }
-                if(curr.left!=null){
-                    q.add(curr.left);
-                    q.add(curr.right);
-                }
+                current = current.next;
             }
+            level = level.left;  
         }
         return root;
     }
