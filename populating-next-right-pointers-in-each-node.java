856e1c43855541eaf1c09b7f930e00dcb2d9f8b4
sharanya-t <sharanyat@Sharanyas-MacBook-Pro.local>
2023-12-31 18:40:32 +0530

    Please review

---
+++ populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,50 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : -
+
+
+// Your code here along with comments explaining your approach
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Node level = root;
+        while(level != null){
+            Node curr = level;
+            while(curr != null){
+                if(curr.left != null){
+                    curr.left.next = curr.right;
+                }
+                if(curr.right != null && curr.next != null){
+                    curr.right.next = curr.next.left;
+                }
+                curr = curr.next;
+            }
+            level = level.left;
+        }
+        return root;
+    }
+}
